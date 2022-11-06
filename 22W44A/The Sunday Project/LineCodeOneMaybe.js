5c655e6e9d9d2925208c86e912822a93e9686a46
Seth <seth@bytech.ca>
2022-11-06 15:49:52 +0100

    Done

--- 22W44A/The Sunday Project/LineCodeOneMaybe.js
+++ 22W44A/The Sunday Project/LineCodeOneMaybe.js
@@ -1,4 +1,10 @@
 const timeElapsed = Date.now();
 const today = new Date(timeElapsed);
+theDateToday = today.toDateString();
+theDay = theDateToday.substr(0,3)
 
-console.log (today)
+if (theDay=='Sun'){
+    console.log ("It's Sunday Bitches")
+}   else{
+    console.log ("It's not sunday")
+}
