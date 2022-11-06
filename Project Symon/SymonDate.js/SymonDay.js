3de2ff88ea9d0645b40efd73c61444ab54a22e20
Seth <seth@bytech.ca>
2022-11-06 17:38:30 +0100

    p

---
+++ Project Symon/SymonDate.js/SymonDay.js
@@ -0,0 +1,25 @@
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+theDateToday = today.toDateString();
+
+
+function whatDayIsIt(theDayToday){
+    theDay = theDateToday.substr(0,3)
+    return theDay
+}
+
+function whatMonthisit(theDateToday){
+    theMonth = theDateToday.substr(4,3)
+    return theMonth
+}
+
+function whatDayOfTheMonthIsIt(theDateToday){
+    theDayOfTheMonth = theDateToday.substr(8,2)
+    return theDayOfTheMonth
+}
+
+function whatYearIsIt(theDateToday){
+    theYear = theDateToday.substr(11,4)
+    return theYear
+}
+
